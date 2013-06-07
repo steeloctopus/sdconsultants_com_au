305d4d8421a85b7d4a67fcdb3f980bc161c7abdd
Dominic Milburn <dominic.milburn@nttdata.com>
2013-06-07 14:10:22 +1000

    Implementation of jQuery dynamic elements

--- html-mock-up/js/main.js
+++ html-mock-up/js/main.js
@@ -1,1 +1,42 @@
 
+$(document).ready(function ()
+{
+    $(".case-study-category").bind("click" , getCaseStudies);
+});
+
+function getCaseStudies(evt) {
+    $(".case-study-category").removeClass("chosen");
+
+    var category = $(this).attr('id');
+    var url = category + ".html";
+    $("body").addClass("loading");
+    jQuery.get(url,function(data) {
+        $('.case-studies').html(data);
+    }).done(function() {
+            $("summary:first").attr('id','expanded');
+            $(".case-study").bind("click", displayCaseStudy);
+            $(".case-studies").fadeIn("slow", "linear");
+            $("#expanded").fadeIn("slow", "linear");
+            $("body").removeClass("loading");
+        })
+    .fail(function() {
+                alert("Problem loading page");
+                $("body").removeClass("loading");
+                });
+    $(this).addClass("chosen");
+
+    return false;
+};
+
+
+function displayCaseStudy (evt) {
+
+
+   $("#expanded").slideUp(1000,"swing",true);
+   $("#expanded").attr('id',null);
+   $(".case-study h2").attr('class','plus');
+   $(this).find("summary").slideDown(1000,"swing",true);
+   $(this).find("h2").attr('class','minus');
+   $(this).find("summary").attr('id','expanded');
+
+}
